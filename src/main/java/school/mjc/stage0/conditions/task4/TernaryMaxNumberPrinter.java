01f486accb68fcdb52f48c81789500b25b245321
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-29 13:43:32 +0500

    ternary conditions

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,12 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        if (first == second && first == third){
-            System.out.println(first);
-        } else{
-            System.out.println(first >= second ? (first >= third ? first : third) : second);
-        }
-
-
+        int max = (first >= second && first >= third) ? first : ((second >= first && second >= third) ? second : third);
+        System.out.println(max);
     }
 }
